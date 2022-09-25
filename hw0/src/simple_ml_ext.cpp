c4909b180263dbfe4f6d9327e8c07133a0b94df8
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2022-09-25 20:56:27 +0530

    cpp done :hushhhh :)

--- hw0/src/simple_ml_ext.cpp
+++ hw0/src/simple_ml_ext.cpp
@@ -5,6 +5,14 @@
 
 namespace py = pybind11;
 
+void matrix_multiplication(const float* x, const float* theta, float* z , int n,int k, int m){    
+      for (int i = 0; i < m; i++) {  // matrix multiplication of m X k with k* n
+        for (int g = 0; g < k; g++) {
+            for (int j = 0; j < n; j++) {
+                z[i * n + j] += x[i * k + g] * theta[g * n + j];
+            }
+        }
+    }}
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
@@ -30,14 +38,82 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      *
      * Returns:
      *     (None)
+
+         ### BEGIN YOUR CODE
+      
      */
 
     /// BEGIN YOUR CODE
 
-    
+    //since we dont have data in matrix form; we will have to write the matrix multiplication manually;
+    //also allocate memory from heap to store output 
 
-    
+    //z = np.exp(x@theta)
 
+    auto  z= new float[k*batch] ;// z has shape of [batch, k]; hence this much mem;
+    auto grad = new float [n*k]; // same size as theta
+    //auto  Iy= new float[k*batch];
+    auto x_transpose = new float[batch * n];
+
+
+    for (int I=0; I <m; I+=batch) { //accessing the images with the for loop
+    auto x = X + I * n; //X is a pointer to float
+    auto yi = y + I; //
+
+    // initialise the vals : saw this in a someone else's solution . Very help full
+    std::fill(z, z + batch * k, 0.0f);
+    std::fill(x_transpose, x_transpose + batch * n, 0.0f);
+    std::fill(grad, grad + n * k, 0.0f);
+
+    matrix_multiplication(x, theta, z,k,n, batch); // z contains the matrix multiplication product
+
+    //now we have to take exponentials  n normalise them
+    for (int i=0; i< batch; i++){
+          float summ=0.0f;
+
+      for (int j=0; j<k;j++){
+        z[i*k+j] = exp(z[i*k+j]);
+        summ+= z[i*k + j];
+      }
+
+      for (int j=0;j<k;j++)
+        z[i*k+j] /=summ;
+
+      
+    }
+    //normalisation done :)
+    //Iy matrix :( (i dearly miss numpy)
+    /*
+    for (int i=0; i< batch; i++){
+      //
+      Iy[i*k + y[i]] =1.0f ; //hopefully this works :(
+    }
+
+    for (int i=0; i<batch;i++){
+      z[i*k +yi[i]] -= Iy[i*k+yi[i]]; 
+    }
+    i realised we can just subtract one from the z; no need of iy hehe
+    */
+
+    // Z - Iy done 
+    for (int i=0; i<batch; i++){  
+      z[i * k + yi[i]] -= 1.0f;
+    }
+
+    //store transpose of x in xT
+    for (int i = 0; i < n; i++) {
+            for (int j = 0; j < batch; j++) {
+                x_transpose[i * batch + j] = x[j * n + i];
+            }
+        }
+    
+    matrix_multiplication(x_transpose, z, grad, k, batch, n);
+
+    for (int i=0; i<n*k;i++){
+      theta[i] -= lr * grad[i]/batch;
+    }
+
+    }    
     /// END YOUR CODE
 }
 
